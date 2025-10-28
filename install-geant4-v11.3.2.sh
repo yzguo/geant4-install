5dadb8ff6d57e35345209bd38c2fec9c7b30cb6c
Yanzhen Guo <yzhguo@mail.ustc.edu.cn>
2025-10-28 21:30:22 +0800

    New.

--- install-geant4-v11.3.2.sh
+++ install-geant4-v11.3.2.sh
@@ -4,12 +4,12 @@ set -e
 sudo apt update && sudo apt upgrade -y
 
 # Basic
-sudo apt install -y build-essential cmake wget git curl make gdb libexpat1-dev qtbase5-dev libvtk9-dev libvtk9-qt-dev
+sudo apt install -y build-essential cmake wget git curl axel make gdb libexpat1-dev qtbase5-dev libvtk9-dev libvtk9-qt-dev
 
 mkdir -p $HOME/geant4
 cd $HOME/geant4/
 
-wget https://gitlab.cern.ch/geant4/geant4/-/archive/v11.3.2/geant4-v11.3.2.tar.gz
+axel -n 32 https://gitlab.cern.ch/geant4/geant4/-/archive/v11.3.2/geant4-v11.3.2.tar.gz
 tar -xzf geant4-v11.3.2.tar.gz
 mkdir -p geant4-build geant4-install
 cd geant4-build/
