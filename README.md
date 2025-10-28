9f9d6265d58a6205fb9726de6fc4bfd1969d821e
Yanzhen Guo <yzhguo@mail.ustc.edu.cn>
2025-10-28 16:18:22 +0800

    Nearly done.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# geant4-install
+# Geant4-install
+
 Geant4 在 Linux 及 Docker 中的安装脚本
+
+## 在物理机/虚拟机 (Debian/Ubuntu) 中安装
+
+需要有 sudo 权限或以 root 用户执行如下命令：
+
+```bash
+wget https://github.com/yzguo/geant4-install | sh
+```
+
+该脚本会将 Geant4 安装到 $HOME/geant4/ 目录中。
+
+## 在 Docker 中安装
+
+需要有 sudo 权限或以 root 用户执行如下命令：
+
+```bash
+git clone https://github.com/yzguo/geant4-install.git
+cd geant4-v11.3.2-docker/
+make build
+make run
+```
+
+然后就可以使用用浏览器访问 127.0.0.1:8080 来使用 Geant4 了，也可以在局域网中通过将 127.0.0.1 替换为主机的 IP 地址来访问。
---
+++ install-geant4-v11.3.2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+sudo apt update && sudo apt upgrade -y
+
+# Basic
+sudo apt install -y build-essential cmake wget git curl make gdb libexpat1-dev qtbase5-dev libvtk9-dev libvtk9-qt-dev
+
+mkdir -p $HOME/geant4
+cd $HOME/geant4/
+
+wget https://gitlab.cern.ch/geant4/geant4/-/archive/v11.3.2/geant4-v11.3.2.tar.gz
+tar -xzf geant4-v11.3.2.tar.gz
+mkdir -p geant4-build geant4-install
+cd geant4-build/
+
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/geant4/geant4-install \
+      -DGEANT4_INSTALL_DATA=ON \
+      -DGEANT4_USE_QT=ON \
+      -DGEANT4_USE_VTK=ON \
+      $HOME/geant4/geant4-v11.3.2
+
+make -j$(nproc)
+make install
+
+source $HOME/geant4/geant4-install/bin/geant4.sh
+echo "source $HOME/geant4/geant4-install/bin/geant4.sh" >> $HOME/.bashrc
+
+echo "Geant4 安装完成!请重启终端或执行 source $HOME/.bashrc 生效!"
+
+rm -rf $HOME/geant4/geant4-v11.3.2.tar.gz
+rm -rf $HOME/geant4/geant4-build
