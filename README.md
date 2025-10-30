9f1f51c0b70b3b43b93cf9221d443f0a83aa3dd5
Yanzhen Guo <yzhguo@mail.ustc.edu.cn>
2025-10-30 20:30:46 +0800

    New.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Geant4 在 Linux 及 Docker 中的安装脚本
 wget https://github.com/yzguo/geant4-install/install-geant4-v11.3.2.sh | sh
 ```
 
-该脚本会将 Geant4 安装到 $HOME/geant4/ 目录中。
+该脚本会将 Geant4 安装到 $HOME/geant4/geant4-install 目录中。
 
 ## 在 Docker 中安装
 
@@ -23,4 +23,4 @@ make build
 make run
 ```
 
-然后就可以使用用 ssh -p 2223@localhoost 来使用 Geant4 了，也可以在局域网中通过将 127.0.0.1 替换为主机的 IP 地址来访问。
+然后就可以使用用 ssh -p 2223@localhoost 来使用 Geant4 了，也可以在局域网中通过将 localhost 替换为主机的 IP 地址来访问。
--- install-geant4-v11.3.2.sh
+++ install-geant4-v11.3.2.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt update && sudo apt upgrade -y
 
 # Basic
-sudo apt install -y build-essential cmake wget git curl axel make gdb libexpat1-dev qtbase5-dev libvtk9-dev libvtk9-qt-dev
+sudo apt install -y build-essential cmake wget git curl ca-certificates axel make gdb libexpat1-dev qtbase5-dev libvtk9-dev libvtk9-qt-dev
 
 mkdir -p $HOME/geant4
 cd $HOME/geant4/
@@ -25,8 +25,12 @@ make install
 
 source $HOME/geant4/geant4-install/bin/geant4.sh
 echo "source $HOME/geant4/geant4-install/bin/geant4.sh" >> $HOME/.bashrc
+echo "export GEANT4_INSTALL_DIR=$HOME/geant4/geant4-install" >> $HOME/.bashrc && \
+echo "source $HOME/geant4/geant4-install/bin/geant4.sh" >> $HOME/.bashrc && \
+echo "export CPATH=\$CPATH:$HOME/geant4/geant4-install/include" >> $HOME/.bashrc && \
+echo "export LIBRARY_PATH=\$LIBRARY_PATH:$HOME/geant4/geant4-install/lib" >> $HOME/.bashrc && \
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:$HOME/geant4/geant4-install/lib" >> $HOME/.bashrc
 
 echo "Geant4 Installed in $HOME/geant4/geant4-install, please restart your terminal or run 'source ~/.bashrc' to use Geant4."
 
-rm -rf $HOME/geant4/geant4-v11.3.2.tar.gz
-rm -rf $HOME/geant4/geant4-build
+rm -rf $HOME/geant4/geant4-v11.3.2.tar.gz $HOME/geant4/geant4-build
